fae298d7c217f1e6b858fa07ce35ae32a5c084ed
mgfj2009 <mgfj2009@hotmail.com>
2021-06-16 19:10:18 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import "core-js/stable";
-import "regenerator-runtime/runtime";
 
 ReactDOM.render(<App />, document.querySelector("#root"));
-
-
-
